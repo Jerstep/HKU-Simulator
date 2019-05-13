695344ce949b9cf26b18aaa89af1b4f9ea4bc5c7
Jeremy <jeremy.koskamp@live.nl>
2019-05-13 22:15:41 +0200

    [+] Movement base

---
+++ AllNighter/Assets/Scripts/InputManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager instance = null;
+
+    private PlayerMovement playerMovement;
+
+    void Awake()
+    {
+        if(instance == null)
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if(instance != this)
+            Destroy(gameObject);
+
+        playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+    }
+}
---
+++ AllNighter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public bool moveForward;
+    public bool moveBackward;
+    public bool moveLeft;
+    public bool moveRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
