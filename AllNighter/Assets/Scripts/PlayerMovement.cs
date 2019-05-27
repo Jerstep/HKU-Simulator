96d90a2bb7f864b5e386b67693066977cbd6aedb
Jeremy <jeremy.koskamp@live.nl>
2019-05-27 15:13:38 +0200

    [+] Third Person Char [+] jiggle Bones

--- AllNighter/Assets/Scripts/PlayerMovement.cs
+++ AllNighter/Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public bool grounded;
+    public int speed = 40;
 
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody rb;
+    public Camera camera;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if(InputManager.GetKeyDown(""))
-        {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-        }
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddRelativeForce(movement * speed);
+
+        Look();
+    }
+
+    public float lookSpeed = 3;
+    private Vector2 rotation = Vector2.zero;
+    public void Look() // Look rotation (UP down is Camera) (Left right is Transform rotation)
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+        rotation.x = Mathf.Clamp(rotation.x, -15f, 15f);
+        transform.eulerAngles = new Vector2(0, rotation.y) * lookSpeed;
+        camera.transform.localRotation = Quaternion.Euler(rotation.x * lookSpeed, 0, 0);
     }
 }
