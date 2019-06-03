43e126d018c2c4f0476e7098dcf394de33a1f000
Unknown <jeremy.koskamp@live.nl>
2019-06-04 01:23:50 +0200

    [+] UI

--- AllNighter/Assets/Scripts/GameManager.cs
+++ AllNighter/Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance = null;
+    public static GameManager instance;
     private int previousScene;
 
     void Awake()
